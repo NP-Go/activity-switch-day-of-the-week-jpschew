6f398da8cc92b2cba9d31bb400cd025beffba8bc
jpschew <jps.chew@gmail.com>
2022-03-30 21:18:55 +0800

    added code to check if the day is odd or even

--- dayOfTheWeek.go
+++ dayOfTheWeek.go
@@ -1,5 +1,25 @@
 package main
 
+import (
+	"fmt"
+	"time"
+)
+
 func main() {
 	//Insert code here
+
+	//Now().Weekday() give mon, tue ... sun
+	//Now().Day() give the date of the month
+	day := time.Now().Weekday()
+	fmt.Printf("%v, %T\n", day, day)
+	// fmt.Printf("%v, %T\n", time.Wednesday, time.Wednesday)
+
+	switch day {
+	case time.Monday, time.Wednesday, time.Friday, time.Sunday:
+		fmt.Println("The day is odd")
+	case time.Tuesday, time.Thursday, time.Saturday:
+		fmt.Println("The day is even")
+	default:
+		fmt.Print("No matching day")
+	}
 }
